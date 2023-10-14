518570850abb5d95365f6ec4fe7dd789d1993a6d
Rafael <rafaelmagalhaes1@live.com>
2023-10-14 09:34:08 -0300

    feat: create request API

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import './App.css';
 import Table from './components/Table/Table';
+import { State } from './types';
 
 class App extends React.Component {
+  state: State = {
+    planets: [],
+  };
+
+  async componentDidMount() {
+    const urlAPI = 'https://swapi.dev/api/planets';
+
+    const response = await fetch(urlAPI);
+    const data = await response.json();
+
+    this.setState({ planets: data.results });
+    console.log(data.results);
+  }
+
   render() {
+    const { planets } = this.state;
+
     return (
       <>
         <div>Hello, App!</div>
-        <Table />
+        <Table planets={ planets } />
       </>
     );
   }
