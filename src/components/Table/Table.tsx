ac087b59f3edcea560542a0b9bf0422250e1cbf7
Rafael <rafaelmagalhaes1@live.com>
2023-10-14 09:34:56 -0300

    feat: creates table with the returned data

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,9 +1,52 @@
 import React from 'react';
+import { PlanetType } from '../../types';
 
-class Table extends React.Component {
+type TableProps = {
+  planets: PlanetType[];
+};
+
+class Table extends React.Component<TableProps> {
   render() {
+    const { planets } = this.props;
     return (
-      <span>Table</span>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {planets.map((planet) => (
+            <tr key={ planet.edited }>
+              <td>{ planet.name }</td>
+              <td>{ planet.rotation_period }</td>
+              <td>{ planet.orbital_period }</td>
+              <td>{ planet.diameter }</td>
+              <td>{ planet.climate }</td>
+              <td>{ planet.gravity }</td>
+              <td>{ planet.terrain }</td>
+              <td>{ planet.surface_water }</td>
+              <td>{ planet.population }</td>
+              <td>{ planet.films }</td>
+              <td>{ planet.created }</td>
+              <td>{ planet.edited }</td>
+              <td>{ planet.url }</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     );
   }
 }
