14bac293d38654d08d77678beb3df5822afa99c9
Rafael <rafaelmagalhaes1@live.com>
2023-10-14 09:33:17 -0300

    feat: create PlanetType

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+export type PlanetType = {
+  name: string,
+  climate: string,
+  created: string,
+  diameter: string,
+  edited: string,
+  films: string[],
+  gravity: string,
+  orbital_period: string,
+  population: string,
+  rotation_period: string,
+  surface_water: string,
+  terrain: string,
+  url: string,
+};
+
+export type State = {
+  planets: PlanetType[],
+};
