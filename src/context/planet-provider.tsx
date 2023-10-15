35c8813b7e5e32f795eec779f821d4885ae94345
Rafael <rafaelmagalhaes1@live.com>
2023-10-15 10:02:17 -0300

    feat: add filter planets

--- src/context/planet-provider.tsx
+++ src/context/planet-provider.tsx
@@ -8,6 +8,14 @@ type PlanetContextProps = {
 
 function PlanetProvider({ children } : PlanetContextProps) {
   const [planets, setPlanets] = useState<PlanetType[]>([]);
+  const [search, setSearch] = useState('');
+
+  const handleChange = (term: string) => {
+    setSearch(term);
+  };
+
+  const filterPlanets = planets
+    .filter((planet) => planet.name.toLowerCase().includes(search.toLowerCase()));
 
   useEffect(() => {
     async function fetchApi() {
@@ -22,7 +30,7 @@ function PlanetProvider({ children } : PlanetContextProps) {
   }, []);
 
   return (
-    <PlanetContext.Provider value={ { planets } }>
+    <PlanetContext.Provider value={ { planets, search, handleChange, filterPlanets } }>
       {children}
     </PlanetContext.Provider>
   );
