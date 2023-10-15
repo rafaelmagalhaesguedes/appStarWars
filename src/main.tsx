81a599060b6e23ab926e04fdcb9fa33fbda56e22
Rafael <rafaelmagalhaes1@live.com>
2023-10-15 09:46:36 -0300

    feat: add PlanetProvider wrapper App

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import PlanetProvider from './context/planet-provider';
 
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLElement)
-  .render(<App />);
+  .render(
+    <PlanetProvider>
+      <App />
+    </PlanetProvider>,
+  );
