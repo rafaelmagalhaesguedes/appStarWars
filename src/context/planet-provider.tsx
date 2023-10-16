50e1b556fa7664a7b920e235ab6284b8d5127fda
Rafael <rafaelmagalhaes1@live.com>
2023-10-16 13:23:26 -0300

    feat: add filter operation on Planet Provider

--- src/context/planet-provider.tsx
+++ src/context/planet-provider.tsx
@@ -1,32 +1,69 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import { PlanetContext } from './planet-context';
-import { PlanetType } from '../types';
+import { FilterType, PlanetType } from '../types';
 
 type PlanetContextProps = {
   children: React.ReactNode;
 };
 
-function PlanetProvider({ children } : PlanetContextProps) {
+function PlanetProvider({ children }: PlanetContextProps) {
   const [planets, setPlanets] = useState<PlanetType[]>([]);
   const [search, setSearch] = useState('');
-  const [filterPlanets, setFilterPlanets] = useState<PlanetType[]>([]);
+  const [filterPlanets, setFilterPlanets] = useState<PlanetType[]>(planets);
+  const [filterConfig, setFilterConfig] = useState<FilterType[]>([]);
 
-  const handleSearch = (searchTerm: string) => {
-    setSearch(searchTerm);
+  // Carrega o termo de pesquisa
+  const handleSearch = (term: string) => {
+    setSearch(term);
   };
 
+  // Filtra de acordo com o termo
   const handleFilterSearch = useCallback(() => {
-    const filter = planets
+    const filtered = planets
       .filter((planet) => planet.name.toLowerCase().includes(search.toLowerCase()));
-    if (filter) {
-      setFilterPlanets(filter);
-    }
+
+    setFilterPlanets(filtered);
   }, [planets, search]);
 
+  // Carrega o filtro a cada interação
   useEffect(() => {
     handleFilterSearch();
   }, [handleFilterSearch]);
 
+  // Carrega os dados da comparação
+  const handleFilterChange = (filter: FilterType) => {
+    setFilterConfig([...filterConfig, filter]);
+  };
+
+  // Filtra os dados por comparação
+  const applyFilter = useCallback(() => {
+    let filtered = planets;
+    filterConfig.forEach(({ comparison, column, value }) => {
+      filtered = filtered.filter((planet) => {
+        const planetValue = parseFloat(planet[column]);
+        const valueFilter = Number(value);
+
+        if (comparison === 'maior que') {
+          return planetValue > valueFilter;
+        }
+        if (comparison === 'menor que') {
+          return planetValue < valueFilter;
+        }
+        if (comparison === 'igual a') {
+          return planetValue === valueFilter;
+        }
+        return true;
+      });
+    });
+    setFilterPlanets(filtered);
+  }, [planets, filterConfig]);
+
+  // Carrega o filtro a cada interação
+  useEffect(() => {
+    applyFilter();
+  }, [applyFilter, filterConfig]);
+
+  // Carrega os dados da API
   useEffect(() => {
     async function fetchApi() {
       const urlAPI = 'https://swapi.dev/api/planets';
@@ -40,7 +77,15 @@ function PlanetProvider({ children } : PlanetContextProps) {
   }, []);
 
   return (
-    <PlanetContext.Provider value={ { planets, search, handleSearch, filterPlanets } }>
+    <PlanetContext.Provider
+      value={ {
+        planets,
+        search,
+        handleSearch,
+        filterPlanets,
+        handleFilterChange,
+      } }
+    >
       {children}
     </PlanetContext.Provider>
   );
