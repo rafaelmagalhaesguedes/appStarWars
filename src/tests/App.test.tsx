37c556cf44ff8bbf8d845c96d4bc1da9572cd19a
Rafael <rafaelmagalhaes1@live.com>
2023-10-17 20:19:31 -0300

    feat: add Provider App.test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
+import Provider from '../context/planet-provider';
 import FilterForm from '../components/FilterForm/FilterForm';
 import SearchForm from '../components/SearchForm/SearchForm';
 import Table from '../components/Table/Table';
@@ -13,30 +14,39 @@ describe('Tests component SearchForm', () => {
 
 describe('Tests component FilterForm', () => {
   test('Verifica se o form renderiza com os valores iniciais corretos', () => {
-    render(<FilterForm />);
+    render(
+      <Provider>
+        <FilterForm />
+      </Provider>
+    );
     expect(screen.getByTestId('column-filter')).toHaveValue('population');
     expect(screen.getByTestId('comparison-filter')).toHaveValue('maior que');
     expect(screen.getByTestId('value-filter')).toHaveValue(0);
   });
 
   test('Verifica se renderiza o botão Filtrar', () => {
-    render(<FilterForm />);
+    render(
+      <Provider>
+        <FilterForm />
+      </Provider>
+    );
     const filterButton = screen.getByRole('button', { name: 'Filtrar' });
     expect(filterButton).toBeInTheDocument();
   });
 
   test('Verifica se atualiza os dados do formulário', () => {
-    render(<FilterForm />);
+    render(
+      <Provider>
+        <FilterForm />
+      </Provider>
+    );
     
-    // Altera entrada column
     fireEvent.change(screen.getByTestId('column-filter'), { target: { value: 'orbital_period' } });
     expect(screen.getByTestId('column-filter')).toHaveValue('orbital_period');
 
-    // Altera entrada comparison
     fireEvent.change(screen.getByTestId('comparison-filter'), { target: { value: 'menor que' } });
     expect(screen.getByTestId('comparison-filter')).toHaveValue('menor que');
 
-    // Altera entrada para value
     fireEvent.change(screen.getByTestId('value-filter'), { target: { value: '100' } });
     expect(screen.getByTestId('value-filter')).toHaveValue(100);
   });
