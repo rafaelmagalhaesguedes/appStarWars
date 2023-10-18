ca3e4673e5ed905e920aca0b911a6ba0162b9cad
Rafael <rafaelmagalhaes1@live.com>
2023-10-18 12:32:04 -0300

    feat: add mock data and create tests component FilterForm

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,10 +1,20 @@
 import { render, screen, fireEvent } from '@testing-library/react';
+import { vi } from 'vitest';
+import mock from './mock';
 import Provider from '../context/planet-provider';
 import FilterForm from '../components/FilterForm/FilterForm';
 import SearchForm from '../components/SearchForm/SearchForm';
 import Table from '../components/Table/Table';
+import App from '../App';
 
 describe('Tests component SearchForm', () => {  
+
+  const mockFetching = {
+    json: async () => mock,
+  } as Response;
+
+  const mockFetch = vi.spyOn(global, 'fetch').mockResolvedValue(mockFetching)
+
   test('Verifica se o input de pesquisa é renderizado', () => {
     render(<SearchForm />);
     const textInput = screen.getByTestId('name-filter');
@@ -13,6 +23,66 @@ describe('Tests component SearchForm', () => {
 });
 
 describe('Tests component FilterForm', () => {
+  test('Verifica se os campos do formulário podem ser preenchidos e enviados corretamente', () => {
+    render(
+      <Provider>
+        <FilterForm />
+      </Provider>
+    );
+  
+    const columnFilter = screen.getByTestId('column-filter');
+    const comparisonFilter = screen.getByTestId('comparison-filter');
+    const valueFilter = screen.getByTestId('value-filter');
+  
+    fireEvent.change(columnFilter, { target: { value: 'diameter' } });
+    fireEvent.change(comparisonFilter, { target: { value: 'maior que' } });
+    fireEvent.change(valueFilter, { target: { value: '5000' } });
+  
+    expect(columnFilter).toHaveValue('diameter');
+    expect(comparisonFilter).toHaveValue('maior que');
+    expect(valueFilter).toHaveValue(5000);
+
+    const filterButton = screen.getByTestId('button-filter');
+    fireEvent.click(filterButton);
+  
+    const filters = screen.getAllByTestId('filter');
+    expect(filters.length).toBe(1);
+    expect(screen.getByText('diameter')).toBeInTheDocument();
+  
+    const comparisonTextElements = screen.queryAllByText('maior que');
+    expect(comparisonTextElements.length).toBe(2);
+
+    expect(valueFilter).toHaveValue(0);
+  });  
+
+  test('Verifica se a remoção de filtros funciona corretamente', () => {
+    render(
+      <Provider>
+        <FilterForm />
+      </Provider>
+    );
+
+    const columnFilter = screen.getByTestId('column-filter');
+    const comparisonFilter = screen.getByTestId('comparison-filter');
+    const valueFilter = screen.getByTestId('value-filter');
+
+    fireEvent.change(columnFilter, { target: { value: 'diameter' } });
+    fireEvent.change(comparisonFilter, { target: { value: 'maior que' } });
+    fireEvent.change(valueFilter, { target: { value: '5000' } });
+
+    const filterButton = screen.getByTestId('button-filter');
+    fireEvent.click(filterButton);
+
+    const filters = screen.getAllByTestId('filter');
+    expect(filters.length).toBe(1);
+
+    const removeFilterButton = screen.getByTestId('button-remove-filters');
+    fireEvent.click(removeFilterButton);
+
+    const removedFilters = screen.queryAllByTestId('filter');
+    expect(removedFilters.length).toBe(0);
+  });
+  
   test('Verifica se o form renderiza com os valores iniciais corretos', () => {
     render(
       <Provider>
@@ -24,14 +94,16 @@ describe('Tests component FilterForm', () => {
     expect(screen.getByTestId('value-filter')).toHaveValue(0);
   });
 
-  test('Verifica se renderiza o botão Filtrar', () => {
+  test('Verifica se renderiza o botão Filtrar e Remover', () => {
     render(
       <Provider>
         <FilterForm />
       </Provider>
     );
     const filterButton = screen.getByRole('button', { name: 'Filtrar' });
+    const removeButton = screen.getByRole('button', { name: 'Remover Filter' });
     expect(filterButton).toBeInTheDocument();
+    expect(removeButton).toBeInTheDocument();
   });
 
   test('Verifica se atualiza os dados do formulário', () => {
