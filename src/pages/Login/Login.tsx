e4b92900db9b6fae24953eb6e65126df2aaa1e2a
Rafael <rafaelmagalhaes1@live.com>
2023-10-21 10:05:09 -0300

    feat: create Login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,17 @@
+import { Link } from 'react-router-dom';
+import { Form, Image, LoginContainer, Logo } from './Styles';
+import logo from '../../assets/imgs/logo-star wars.png';
+
 function Login() {
   return (
-    <div>Login</div>
+    <LoginContainer>
+      <Logo>
+        <Image src={ logo } alt="" />
+      </Logo>
+      <Form>
+        <Link to="/home">Explore Planets</Link>
+      </Form>
+    </LoginContainer>
   );
 }
 
