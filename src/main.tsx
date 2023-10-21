bf67e037f40abd7a40ab0d4aaf59a49331bb44dc
Rafael <rafaelmagalhaes1@live.com>
2023-10-21 10:04:50 -0300

    feat: install styled components and create Global Style

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import PlanetProvider from './context/PlanetProvider';
 import App from './App';
+import { GlobalStyle } from './assets/css/GlobalStyle';
 
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLElement)
   .render(
-    <BrowserRouter>
-      <PlanetProvider>
+    <PlanetProvider>
+      <BrowserRouter>
+        <GlobalStyle />
         <App />
-      </PlanetProvider>
-    </BrowserRouter>,
+      </BrowserRouter>
+    </PlanetProvider>,
   );
